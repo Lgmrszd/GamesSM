79097c00871e0ac982f434f45311e904452870b9
Mikhail <m.v.bobrov@yandex.ru>
2018-06-30 22:59:47 +0300

    gui testing

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListView, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListView, QTableView, QPushButton
 from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
 from PyQt5.QtCore import QStringListModel
+from PyQt5.QtSql import QSqlQueryModel
 from PyQt5 import uic
 
 
@@ -15,7 +16,8 @@ class GSMMainWindow(QMainWindow):
         self.restoreButton = self.findChild(QPushButton, "restoreButton")
 
         self.pluginsList = self.findChild(QListView, "pluginsList")
-        # self.pluginsList.setEditTriggers(QListView.NoEditTriggers)
+        self.savedSlots = self.findChild(QTableView, "savedSlots")
+        self.gameSlots = self.findChild(QListView, "gameSlots")
 
         self.setWindowTitle("GamesSM")
         self.show()
@@ -30,5 +32,13 @@ if __name__ == '__main__':
     for i in range(50):
         m.appendRow(QStandardItem(QIcon.fromTheme("edit-undo"), str(i)))
     w.pluginsList.setModel(m)
+
+    assert isinstance(w.savedSlots, QTableView)
+    m2 = QStandardItemModel()
+    m2.setHorizontalHeaderLabels(["a", "b"])
+    m2.setVerticalHeaderLabels(["a", "b"])
+    m2.columnsAboutToBeMoved()
+    w.savedSlots.setModel(m2)
+    # w.pluginsList.setModel(m2)
 
     sys.exit(app.exec_())
