5c36f9ca5e4b5ff176c95f677e7850be196ec903
Mikhail <m.v.bobrov@yandex.ru>
2018-07-03 14:01:19 +0300

    moved BaseModel to db_connect, Plugin model to plugins

--- db_connect.py
+++ db_connect.py
@@ -1,3 +1,8 @@
 import peewee as pw
 
 db = pw.SqliteDatabase("data.db")
+
+
+class BaseModel(pw.Model):
+    class Meta:
+        database = db
--- models.py
+++ models.py
@@ -1,24 +1,11 @@
 import peewee as pw
 from datetime import datetime
-from db_connect import db
+from db_connect import db, BaseModel
 
 
-class BaseModel(pw.Model):
-    class Meta:
-        database = db
 
 
-class Plugin(BaseModel):
-    name = pw.CharField(max_length=50, primary_key=True)
-    description = pw.CharField()
-    enabled = pw.BooleanField(default=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.broken = False
-
-
-class SaveSlot(BaseModel):
-    date = pw.DateTimeField(default=datetime.now)
-    plugin = pw.ForeignKeyField(Plugin, backref="saves", null=True)
-    data = pw.BlobField()
+# class SaveSlot(BaseModel):
+#     date = pw.DateTimeField(default=datetime.now)
+#     plugin = pw.ForeignKeyField(Plugin, backref="saves", null=True)
+#     data = pw.BlobField()
