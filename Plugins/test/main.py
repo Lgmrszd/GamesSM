ee08e048a918bbe8d41a5bbbddc2c85674db7949
Lgmrszd <m.v.bobrov@yandex.ru>
2018-07-04 22:52:39 +0300

    New plugin structure

--- Plugins/test/main.py
+++ Plugins/test/main.py
@@ -1,6 +1,9 @@
-FIXED_SLOTS = True
-SLOTS_NUMBER = 10
+from plugins import PluginModule
 
 
-def main():
-    print("TEST!")
+# TODO: more appropriate name
+class PluginMainModule(PluginModule):
+    def __init__(self):
+        super().__init__()
+        self.FIXED_SLOTS = True
+        self.MAX_SLOTS_NUMBER = 10
