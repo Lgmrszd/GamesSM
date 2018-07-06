916ef4d99ebd01196d0b1af7bcc5bc625d7970ed
Mikhail <m.v.bobrov@yandex.ru>
2018-07-06 17:13:39 +0300

    New gui changes (open plugins & restart button)

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QTableWidget, QTableWidgetItem, QPushButton, QAction, QLabel
+import PyQt5.QtWidgets
 from PyQt5.QtGui import QColor, QBrush
 from PyQt5 import uic
-from PyQt5.QtCore import QObject, Qt
+from PyQt5.QtCore import QObject, QCoreApplication
 import plugins
+
+EXIT_CODE_RESTART = -123
 
 
 class PluginSettingsRow(QObject):
     columns = ["Name", "Enabled"]
 
-    class PluginWidgetItem(QTableWidgetItem):
+    class PluginWidgetItem(PyQt5.QtWidgets.QTableWidgetItem):
         def __init__(self, plugin_row, *args):
             super().__init__(*args)
             self.plugin_row = plugin_row
@@ -39,27 +41,30 @@ class PluginSettingsRow(QObject):
     def update_status(self):
         self.enab_widg.setText(self._str_enabled())
 
-    def insert_into_table(self, table: QTableWidget):
+    def insert_into_table(self, table: PyQt5.QtWidgets.QTableWidget):
         r = table.rowCount()
         table.insertRow(r)
         table.setItem(r, 0, self.name_widg)
         table.setItem(r, 1, self.enab_widg)
 
 
-class SettingsWindow(QMainWindow):
+class SettingsWindow(PyQt5.QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("ui/Settings.ui", self)
 
         self.current_plugin = None  # type: plugins.Plugin
 
-        self.pluginLabel: QLabel = self.findChild(QLabel, "pluginLabel")
+        self.pluginLabel: PyQt5.QtWidgets.QLabel = self.findChild(PyQt5.QtWidgets.QLabel, "pluginLabel")
 
-        self.toggleButton: QPushButton = self.findChild(QPushButton, "toggleButton")
+        self.toggleButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "toggleButton")
         self.toggleButton.setDisabled(True)
         self.toggleButton.clicked.connect(self.on_toggle_button_clicked)
 
-        self.pluginsTable: QTableWidget = self.findChild(QTableWidget, "pluginsTable")
+        self.restartButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "restartButton")
+        self.restartButton.clicked.connect(self.on_restart_button_clicked)
+
+        self.pluginsTable: PyQt5.QtWidgets.QTableWidget = self.findChild(PyQt5.QtWidgets.QTableWidget, "pluginsTable")
 
         # self.pluginsTable.setFixedWidth(300)
         self.pluginsTable.setColumnCount(len(PluginSettingsRow.columns))
@@ -88,6 +93,10 @@ class SettingsWindow(QMainWindow):
             self.toggleButton.setDisabled(False)
             self.toggleButton.setText(self.tr("Enable"))
 
+    def on_restart_button_clicked(self, _):
+        qapp = QCoreApplication.instance()
+        qapp.exit(EXIT_CODE_RESTART)
+
     def on_toggle_button_clicked(self, _):
         self.current_plugin.enabled = not self.current_plugin.enabled
         self.current_plugin.save()
@@ -109,7 +118,7 @@ class SettingsWindow(QMainWindow):
             plugin_row.insert_into_table(self.pluginsTable)
 
 
-class GSMMainWindow(QMainWindow):
+class GSMMainWindow(PyQt5.QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("ui/MainWindow.ui", self)
@@ -118,22 +127,33 @@ class GSMMainWindow(QMainWindow):
 
         self.settingsWindow = SettingsWindow()
 
-        self.copyButton: QPushButton = self.findChild(QPushButton, "copyButton")
-        self.restoreButton: QPushButton = self.findChild(QPushButton, "restoreButton")
-        self.replaceButton: QPushButton = self.findChild(QPushButton, "replaceButton")
+        self.copyButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "copyButton")
+        self.restoreButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "restoreButton")
+        self.replaceButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "replaceButton")
 
-        self.pluginsList: QListWidget = self.findChild(QListWidget, "pluginsList")
+        self.pluginSettingsButton: PyQt5.QtWidgets.QPushButton = self.findChild(PyQt5.QtWidgets.QPushButton, "pluginSettingsButton")
+        self.pluginSettingsButton.clicked.connect(self.on_pluigin_settings_button_clicked)
+
+        self.pluginsList: PyQt5.QtWidgets.QListWidget = self.findChild(PyQt5.QtWidgets.QListWidget, "pluginsList")
         self.pluginsList.itemClicked.connect(self.on_plugin_select)
 
-        self.savedSlots: QTableWidget = self.findChild(QTableWidget, "savedSlots")
-        self.gameSlots: QListWidget = self.findChild(QListWidget, "gameSlots")
+        self.savedSlots: PyQt5.QtWidgets.QTableWidget = self.findChild(PyQt5.QtWidgets.QTableWidget, "savedSlots")
+        self.gameSlots: PyQt5.QtWidgets.QListWidget = self.findChild(PyQt5.QtWidgets.QListWidget, "gameSlots")
 
-        self.pluginLabel: QLabel = self.findChild(QLabel, "pluginLabel")
+        self.pluginLabel: PyQt5.QtWidgets.QLabel = self.findChild(PyQt5.QtWidgets.QLabel, "pluginLabel")
 
-        self.actionSettings: QAction = self.findChild(QAction, "actionSettings")
+        self.actionSettings: PyQt5.QtWidgets.QAction = self.findChild(PyQt5.QtWidgets.QAction, "actionSettings")
         self.actionSettings.triggered.connect(self.settingsWindow.show)
 
         self.load_plugins()
+
+    def on_pluigin_settings_button_clicked(self):
+        show_about_dialog(self)
+        # self.settingsWindow.show()
+        tabWidget: PyQt5.QtWidgets.QTabWidget = self.settingsWindow.findChild(PyQt5.QtWidgets.QTabWidget, "settingsTabWidget")
+        pluginsTab = self.settingsWindow.findChild(PyQt5.QtWidgets.QWidget, "pluginsTab")
+        if tabWidget and pluginsTab:
+            tabWidget.setCurrentWidget(pluginsTab)
 
     def update_label(self):
         text = ("<b>Name</b>: {}<br>"
@@ -151,15 +171,12 @@ class GSMMainWindow(QMainWindow):
         enabled_plugins = plugins.get_enabled_plugins_list()
 
         for enabled_plugin in enabled_plugins:
-            item = QListWidgetItem(enabled_plugin.name)
+            item = PyQt5.QtWidgets.QListWidgetItem(enabled_plugin.name)
             self.pluginsList.addItem(item)
 
     def closeEvent(self, QCloseEvent):
-        QApplication.closeAllWindows()
+        PyQt5.QtWidgets.QApplication.closeAllWindows()
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = GSMMainWindow()
-    w.show()
-    sys.exit(app.exec_())
+def show_about_dialog(parent):
+    PyQt5.QtWidgets.QMessageBox.about(parent, "A", "B")
